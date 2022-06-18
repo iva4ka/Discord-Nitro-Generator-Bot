ab6dfebe6a587d5686751637baea5ee149471ca2
Ocryol <99667441+Ocryol1337@users.noreply.github.com>
2022-06-18 23:58:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,30 +8,12 @@ client = discord.Client()
 
 client = commands.Bot(command_prefix = '!')
 
-
 @client.command()
-async def phonelookup(ctx, *, phonenum):
-    r       = requests.get(f"https://api.numlookupapi.com/v1/validate/{phonenum}?apikey=") # Get an api key from numlookupapi.com and paste it there
-    plookup     = r.json()
-    em      = discord.Embed(color=000000)
-    fields  = [ 
-        {'name': 'Valid:',          'value': plookup['valid']},
-        {'name': 'Number:',     'value': plookup['number']},
-        {'name': 'Local Format:',     'value': plookup['local_format']},
-        {'name': 'International Format:',        'value': plookup['international_format']},
-        {'name': 'Country prefix:',   'value': plookup['country_prefix']},
-        {'name': 'Country Code:',     'value': plookup['country_code']},
-        {'name': 'Country Name:',         'value': plookup['country_name']},
-        {'name': 'Location',    'value': plookup['location']},
-        {'name': 'Linetype:',   'value': plookup['line_type']},
-    ]
-    for field in fields:
-        if field['value']:
-            em.add_field(name=field['name'], value=field['value'], inline=True)
-
-    em.set_footer(text='\u200b')
-    em.timestamp = datetime.datetime.utcnow()  
-    em.set_footer(text='Made By ocryol#8123')
-    await ctx.send(embed = em)
+async def randompasswordgenerator(ctx, *, length: str = '16') -> None:
+  length = int(length)
+  string = lower + upper + numbers + symbols
+  password = "".join(random.sample(string, length))
+  embed = discord.Embed(title="Here is your randomly generated password.", description=f"**{password}**")
+  await ctx.send(embed=embed)
 
 client.run("TOKEN HERE")
