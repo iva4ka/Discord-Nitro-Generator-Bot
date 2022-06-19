baa4271c18fa952cde6ece64a2053d6af60df1d7
Ocryol <99667441+Ocryol1337@users.noreply.github.com>
2022-06-19 11:29:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,12 @@ client = discord.Client()
 client = commands.Bot(command_prefix = '!')
 
 @client.command()
-async def randompasswordgenerator(ctx, *, length: str = '16') -> None:
-  length = int(length)
-  string = lower + upper + numbers + symbols
-  password = "".join(random.sample(string, length))
-  embed = discord.Embed(title="Here is your randomly generated password.", description=f"**{password}**")
+async def nitrogen(ctx):
+  code = "https://discord.gift/" + ('').join(
+	random.choices(string.ascii_letters + string.digits, k=16))
+  embed = discord.Embed(title="Here is the generated nitro link!", description=f"{code}")
+  embed.set_thumbnail(url='https://i.ibb.co/hf4yqpp/Nitro-badge.png')
+  embed.set_footer(text="Source Code: https://github.com/Ocryol1337/Discord-Nitro-Generator-Bot")
   await ctx.send(embed=embed)
 
 client.run("TOKEN HERE")
