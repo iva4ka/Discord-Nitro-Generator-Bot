d1634531d096e8bb0283b68bedd365561049272d
iva <107190556+iva4ka@users.noreply.github.com>
2022-06-29 05:11:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,9 @@ async def nitrogen(ctx):
   code = "https://discord.gift/" + ('').join(
 	random.choices(string.ascii_letters + string.digits, k=16))
   embed = discord.Embed(title="Here is the generated nitro link!", description=f"{code}")
-  embed.set_thumbnail(url='https://i.ibb.co/hf4yqpp/Nitro-badge.png')
-  embed.set_footer(text="Source Code: https://github.com/Ocryol1337/Discord-Nitro-Generator-Bot")
+  embed.set_thumbnail(url='')
+  embed.set_footer(text="iva4ka")
   await ctx.send(embed=embed)
-
-## This "checks" the discord nitro because with the other one you cannot see if the nitro is valid or not!
 
 @client.command()
 async def nitrogen(ctx):
